e1428b11743f24c5643b1e0c490383741c577a1b
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-05-27 09:06:55 -0300

    Initial commit

---
+++ Assets/movementt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementt : MonoBehaviour
+{
+    public float movespeed;
+    public float rotpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movespeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movespeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotpeed, 0);
+        }
+    }
+}
